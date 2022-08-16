e4c1db538787eef7f13641ed98e10bb6ebe7022a
giovannipinna96 <giovanni.pinna.17l96@gmail.com>
2022-08-16 17:28:14 +0200

    bug correction

--- ii_test.py
+++ ii_test.py
@@ -78,7 +78,6 @@ def main():
         print(f"Testing model - {(1-test_non_ood.int()).abs().sum().item()} samples removed from testset")
         filtered_testset = utils.subset_imagefolder(testloader.dataset, test_non_ood)
         filtered_testloader = torch.utils.data.DataLoader(filtered_testset, batch_size=args.batch_size, shuffle=False, num_workers=4)
-        print(next(iter(testloader))[1],"\n", next(iter(filtered_testloader))[1])
         eval_ii.test_model(net, filtered_testloader, device=args.device)
 
 if __name__ == "__main__":
--- punches_lib/ii_loss/eval.py
+++ punches_lib/ii_loss/eval.py
@@ -115,6 +115,7 @@ def test_model(model:torch.nn.Module, dataloader:torch.utils.data.DataLoader, lo
     with torch.no_grad():
         for X, y in dataloader:
             X = X.to(device)
+            print(y)
             y = torch.Tensor(y).to(device)
             
             _, y_hat = model(X)
