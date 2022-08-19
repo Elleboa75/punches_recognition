1beae523c2c69bca4d9a974a693309261260d2b3
giovannipinna96 <giovanni.pinna.17l96@gmail.com>
2022-08-19 12:02:16 +0200

    bug correction

--- punches_lib/gan/models_experimental.py
+++ punches_lib/gan/models_experimental.py
@@ -2,58 +2,56 @@ import torch
 from torch import nn
 
 class Generator(nn.Module):
-    def __init__(self, ngpu=1, nz=100, ngf=64, nc=512):
+    def __init__(self, latent_dim=100, base_width=64, num_channels=512):
         super(Generator, self).__init__()
-        self.ngpu = ngpu
-        self.nz = nz
-        self.ngf = ngf
-        self.nc = nc
+        self.latent_dim = latent_dim
+        self.base_width = base_width
+        self.num_channels = num_channels
         self.main = nn.Sequential(
             # input is Z, going into a convolution
             # Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-            nn.Conv2d( self.nz, self.ngf * 8, 1, 1, 1, bias=False),
-            nn.BatchNorm2d(self.ngf * 8),
+            nn.Conv2d( self.latent_dim, self.base_width * 8, 1, 1, 1, bias=False),
+            nn.BatchNorm2d(self.base_width * 8),
             nn.ReLU(True),
-            # state size. (self.ngf*8) x 4 x 4
-            nn.Conv2d(self.ngf * 8, self.ngf * 4, 1, 2, 0, bias=False),
-            nn.BatchNorm2d(self.ngf * 4),
+            # state size. (self.base_width*8) x 4 x 4
+            nn.Conv2d(self.base_width * 8, self.base_width * 4, 1, 2, 0, bias=False),
+            nn.BatchNorm2d(self.base_width * 4),
             nn.ReLU(True),
-            # state size. (self.ngf*4) x 8 x 8
-            nn.Conv2d( self.ngf * 4, self.ngf * 2, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(self.ngf * 2),
+            # state size. (self.base_width*4) x 8 x 8
+            nn.Conv2d( self.base_width * 4, self.base_width * 2, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.base_width * 2),
             nn.ReLU(True),
-            # state size. (self.ngf*2) x 16 x 16
-            nn.Conv2d( self.ngf * 2, self.ngf*4, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(self.ngf*4),
+            # state size. (self.base_width*2) x 16 x 16
+            nn.Conv2d( self.base_width * 2, self.base_width*4, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.base_width*4),
             nn.ReLU(True),
-            # state size. (self.ngf) x 32 x 32
-            nn.Conv2d( self.ngf*4, self.nc, 1, 1, 0, bias=True),
+            # state size. (self.base_width) x 32 x 32
+            nn.Conv2d( self.base_width*4, self.num_channels, 1, 1, 0, bias=True),
             #nn.Tanh()
-            # state size. (self.nc) x 64 x 64
+            # state size. (self.num_channels) x 64 x 64
         )
     def forward(self, input):
         return self.main(input)
     
 class Discriminator(nn.Module):
-    def __init__(self, ngpu=1, nc=512, ndf=64):
+    def __init__(self, num_channels=512, base_width=64):
         super(Discriminator, self).__init__()
-        self.ngpu = ngpu
-        self.nc = nc
-        self.ndf = ndf
+        self.num_channels = num_channels
+        self.base_width = base_width
         self.main = nn.Sequential(
             nn.MaxPool2d(4, 4),
-            nn.Conv2d(self.nc, self.ndf*8, 1, 1, 0, bias=False),
+            nn.Conv2d(self.num_channels, self.base_width*8, 1, 1, 0, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.ndf*8, self.ndf*4, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(self.ndf*4),
+            nn.Conv2d(self.base_width*8, self.base_width*4, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.base_width*4),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.ndf*4, self.ndf*2, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(self.ndf*2),
+            nn.Conv2d(self.base_width*4, self.base_width*2, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.base_width*2),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.ndf*2, self.ndf, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(self.ndf),
+            nn.Conv2d(self.base_width*2, self.base_width, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(self.base_width),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.ndf, 1, 1, 1, 0, bias=False),
+            nn.Conv2d(self.base_width, 1, 1, 1, 0, bias=False),
             nn.Sigmoid()
         )
     def forward(self, input):
